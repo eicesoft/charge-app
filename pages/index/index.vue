0b2dfa1755c3825679996616f96c1258abbb4433
kelezyb <eicesoft@gmail..com>
2023-02-25 12:44:05 +0800

    init project

---
+++ pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <view class="container">
+    <view class="intro"
+      >本项目已包含uni
+      ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view
+    >
+    <text class="intro">详见：</text>
+    <uni-link :href="href" :text="href"></uni-link>
+    <uni-badge size="28">sagasgsagsags</uni-badge>
+    <uni-rate value="3"></uni-rate>
+  </view>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const href = ref('https://uniapp.dcloud.io/component/README?id=uniui&time=' + new Date().getTime());
+</script>
+
+<style lang="scss">
+.container {
+  padding: 40px;
+  font-size: 14px;
+  line-height: 24px;
+}
+</style>
